82e00d8f3bc43397bf631cbd4c23fa10be551589
TarekSaeed0 <naminode2020@gmail.com>
2025-11-23 10:40:34 +0200

    initial commit

---
+++ src/main/java/com/github/discreteteam/discrete_structures_lab_2/Main.java
@@ -0,0 +1,7 @@
+package com.github.discreteteam.discrete_structures_lab_2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
